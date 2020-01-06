a7d7720fa7997a86e78e7146002501709ac3bf0c
patricoferris <patrickferris17@gmail.com>
2020-01-06 16:32:37 +0000

    Change to using Core.Command

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # RISC-V Benchmarks
+-------------------
 
-A tool for compiling a series of RISC-V OCaml cross-compiling benchmarks. 
+A tool for compiling a series of RISC-V OCaml cross-compiling benchmarks. You will need `dune` to build everything. 
+
+Here is an example usage: 
+
+```
+git clone https://github.com/patricoferris/riscv-benchmarks.git
+cd riscv-benchmarks/src 
+
+dune exec -- ./bench.exe build -c <which ever compiler> -args "arg1 arg2" -v
+```
---
+++ src/bench.ml
@@ -0,0 +1,67 @@
+let helpers =
+  ["utils.ml"]
+
+let benchmarks = 
+  ["intfloatarray.ml"; "sorting.ml"]
+
+(* Converting list of files *)
+let change_filetype filetype files =
+  let change_end str ft = 
+    let s = List.hd (String.split_on_char '.' str) in 
+      s ^ ft
+  in
+  let rec loop ft acc = function
+    | [] -> acc 
+    | f::fs -> loop ft ((change_end f ft)::acc) fs
+  in
+    loop filetype [] files
+
+let stringify xs = List.fold_left (fun acc -> fun s -> acc ^ " " ^ s) "" xs  
+
+(* Builds the helpers and benchmarks as cmxs then links - optitional compiler and arguments *)
+let build ?compiler:(compiler="ocamlopt") ?args:(args="") ?verbose:(verbose=false) () =
+  let helper_files = stringify helpers in
+  let benchmark_files = stringify benchmarks in
+  let helper_comp = (compiler ^ " -c " ^ helper_files) in 
+  let benchmark_comp = (compiler ^ " -c " ^ benchmark_files) in 
+    if verbose then (print_endline helper_comp; ignore (Sys.command helper_comp); print_endline benchmark_comp; ignore (Sys.command benchmark_comp))
+    else ignore (Sys.command helper_comp); ignore (Sys.command benchmark_comp);
+  let helper_cmxs = stringify (change_filetype ".cmx" helpers) in
+    List.iter 
+      (fun b -> 
+        let command = (compiler ^ " " ^ args ^ " -o " ^ ((List.hd (String.split_on_char '.' b)) ^ ".out") ^ helper_cmxs ^ " " ^ b) in 
+        if verbose then (print_endline command; ignore (Sys.command command)) else ignore (Sys.command command)
+      ) (change_filetype ".cmx" benchmarks)
+
+let clean ?verbose:(verbose=false) () =
+  let command = "rm *.cmi *.cmx *.o *.out" in 
+  if verbose then (print_endline command; ignore (Sys.command command)) else ignore (Sys.command command)
+
+let run () = 
+  let executables = change_filetype ".out" benchmarks in
+  List.iter (fun exec -> print_endline ("Running " ^ exec ^ " - code: " ^ (string_of_int (Sys.command  ("./" ^ exec))))) executables
+
+(*********** COMMAND LINE TOOL ***********)
+let command = 
+  Core.Command.basic 
+    ~summary:"🐫  RISC-V Benchmark Tool: a CLI for easier compilation 🐫"
+    Core.Command.Let_syntax.(
+      let%map_open 
+            mode = anon (maybe ("mode" %: string))
+        and compiler = flag "-c" (optional string) ~doc:"<compiler> ocamlopt to use"
+        and args     = flag "-args" (optional string) ~doc:"<argument-list> arguments to pass in"
+        and verbose  = flag "-v" no_arg ~doc:" printing build commands etc."
+      in
+        fun () -> 
+          match mode with 
+            | Some "clean"  -> clean ~verbose () 
+            | Some "build"  -> 
+              begin match (compiler, args) with 
+                | (None, None) -> build ~verbose ()
+                | (None, Some args) -> build ~args ~verbose ()
+                | (Some compiler, None) -> build ~compiler ~verbose ()
+                | (Some compiler, Some args) -> build ~compiler ~args ~verbose () end
+            | None | Some _ -> print_endline "Please provide either build or clean as mode"
+    )
+
+let () = Core.Command.run command 
