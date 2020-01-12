43735d062a571b5ff16c92837c051daccd435acc
patricoferris <patrickferris17@gmail.com>
2020-01-12 13:02:44 +0000

    Tail-recurse line reader

--- src/executioner.ml
+++ src/executioner.ml
@@ -2,11 +2,11 @@
 
 let open_file filename = 
   let ic = open_in filename in
-  let rec lines ic = 
+  let rec lines ic acc = 
   try 
     let line = input_line ic in  
-    line :: (lines ic) 
-  with End_of_file -> [] in lines ic          
+    lines ic (line::acc) 
+  with End_of_file -> List.rev acc in lines ic []         
 
 let parse_log log_file = 
   let lines = open_file log_file in 
