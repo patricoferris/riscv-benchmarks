3958a1dca856fb1f49535e50423ac4ecac665691
patricoferris <patrickferris17@gmail.com>
2020-03-26 16:15:18 +0000

    Dunify part IV

--- tests/rd_cycles.c
+++ tests/rd_cycles.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <caml/mlvalues.h>
 
+#ifdef __x86_64__
 uintnat rd_cycles(value unit)
 {
   unsigned long cycles;
@@ -8,3 +9,13 @@ uintnat rd_cycles(value unit)
   asm volatile ("rdtsc" : "=a" (lo), "=d" (hi));
   return ((uint64_t)hi << 32) | lo;
 }
+#endif
+
+#ifdef __riscv
+uintnat rd_cycles(value unit)
+{
+  uintnat cycles;
+  asm volatile ("rdcycle %0" : "=r" (cycles));
+  return cycles;
+}
+#endif
