15487ea9e17b682ef10f61b3d70cca5008bd083e
patricoferris <patrickferris17@gmail.com>
2020-04-18 18:13:38 +0000

    Update shell script

--- ospike.sh
+++ ospike.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-dir="./tests/_build/cross.riscv/riscv/"
+dirr="./tests/_build/cross.riscv/riscv/"
+diro="./tests/_build/cross-original.riscv/riscv" 
 
 range() {
-  name=$dir
+  name=$2
   name+=$(basename $1 .exe)
   name+=".dump"
   # Get object dump of file
@@ -14,13 +15,14 @@ range() {
 
 # Run spike and dump opsikes analysis into file.log 
 spike_ospike () {
-  name=$dir
+  name=$3
   name+=$(basename $1 .exe)
   name+=".log"
   echo $2
   echo "Running spike+ospike for: " $1 
-  spike -l $pk $1 2>&1 | ospike stdin -o $name -lower 0 -upper 0x$2 
+  spike -l  --dc=128:2:64 --ic=128:2:64 $pk $1 2>&1 | ospike stdin -o $name -lower 0 -upper 0x$2 
 }
 
-for i in $dir*.exe; do highest=$(range $i); spike_ospike "$i" "$highest"; done 
+for i in $dirr*.exe; do highest=$(range $i $dirr); spike_ospike "$i" "$highest" "$dirr"; done 
+for i in $diro*.exe; do highest=$(range $i $diro); spike_ospike "$i" "$highest" "$diro"; done 
 
