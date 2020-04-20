a7ce694022a3c765970948e2701f376b69fbd8ee
patricoferris <patrickferris17@gmail.com>
2020-04-20 18:17:01 +0000

    Better yojson testing

--- tests/yojson/datagen.ml
+++ tests/yojson/datagen.ml
@@ -10,29 +10,61 @@ end
 
 type station_record = {
   station_id : string;
+  state : status;
   date : int; 
   data : WeatherData.t;
 }        
+and status = Idle of int | Failure of string | Collecting of int | Maintenance of int  
 
 let pi = 3.14159
 let _ = Random.init 85
 
+(* A simple temperature model for a 24hr period *)
 let temp_gen t mean = 
   7.5 *. sin((pi /. 12.) *. (t -. 6.)) +. mean
 
+(* Data sensor state to JSON string *) 
+let state_to_json = function 
+  | Idle t -> `String ("idle for " ^ (string_of_int t) ^ " minutes") 
+  | Failure s -> `String ("failure - cause: " ^ s)  
+  | Collecting t -> `String ("collecting: " ^ (string_of_int t) ^ " minutes") 
+  | Maintenance t -> `String ("maintenance period:  " ^ (string_of_int t) ^ " minutes")
+
 (* Converts a station_record to json *)
-let to_json {station_id; date; data} = 
+let to_json {station_id; state; date; data} = 
     `Assoc
       [("station_id",`String station_id); 
+       ("status", state_to_json state);
        ("date", `Int date);
        ("data", WeatherData.to_json data)
       ]
+
+let rec find x compare = function 
+  | [] -> None 
+  | (k, v) :: lst -> if compare x k then Some v else find x compare lst  
+
+let replace_if_none = 
+
+let of_json = function 
+  | `Assoc lst -> {
+    station_id = find "station_id" String.equal lst;
+    state = 
+  }
+
+(* Int to state *)
+let int_to_state = function 
+  | 0 -> Idle (Random.int 24)
+  | 1 -> Failure "network error" 
+  | 2 -> Collecting (Random.int 24)
+  | 3 -> Maintenance (Random.int 24)
+  | _ -> assert false 
 
 (* Makes a large list of station_records *)
 let make size = 
   let random_event i = 
     let t = temp_gen (float_of_int i) 10. in
     let temp_min = t -. (Random.float 1.) in 
-    let temp_max = t +. (Random.float 1.) in 
-    let station_id = "S1" in {station_id; date = i; data = {temp_min; temp_max}} in 
+    let temp_max = t +. (Random.float 1.) in
+    let state = Random.int 4 |> int_to_state in 
+    let station_id = "S1" in {station_id; state; date = i; data = {temp_min; temp_max}} in 
   List.init size random_event 
--- tests/yojson/yj.ml
+++ tests/yojson/yj.ml
@@ -1,5 +1,16 @@
 let data = Datagen.make 1_000 
 
+let moving_average win lst = 
+  let rec sum lst = function 
+    | 0 -> 0. 
+    | n -> List.hd lst +. (sum List.tl (n - 1)) in 
+  let rec loop = function 
+    | [] | lst when List.length lst < win -> [] 
+    | lst -> ((sum lst win) /. (float_of_int win)) :: loop (List.tl lst) 
+
+
 let () = 
-  let _json = `List (data |> List.map Datagen.to_json) in 
-  () 
+  (* Generating JSON *)
+  let json  = `List (data |> List.map Datagen.to_json) in 
+  (* Parsing JSON and computing statistics *) 
+  let temps = 
