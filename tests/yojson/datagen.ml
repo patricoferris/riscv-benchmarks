f03094817c4e1eb35a85bf8a1d3ba8f5f8c9a655
patricoferris <patrickferris17@gmail.com>
2020-04-21 09:35:59 +0100

    Add Yojson reading and writing

--- tests/yojson/datagen.ml
+++ tests/yojson/datagen.ml
@@ -1,11 +1,15 @@
-
+module Y = Yojson.Basic 
 module WeatherData = struct 
+  (* Temperature in Celsius 2.d.p as integer *)
   type t = {
-    temp_min : float;
-    temp_max : float; 
+    temp_min : int;
+    temp_max : int; 
   } 
-  let to_json {temp_min; temp_max} = `Assoc [("temp_min", `Float temp_min); ("temp_max", `Float temp_max)]
-  let to_string t = Printf.sprintf "(min %f, max %f)" t.temp_min t.temp_max
+  let of_json = function 
+    | `Assoc [("temp_min", `Int imin); ("temp_max", `Int imax)] -> {temp_min = imin; temp_max = imax}
+    | _ -> assert false
+  let to_json {temp_min; temp_max} = `Assoc [("temp_min", `Int temp_min); ("temp_max", `Int temp_max)]
+  let to_string t = Printf.sprintf "(min %f, max %f)" ((float_of_int t.temp_min) /. 100.) ((float_of_int t.temp_max) /. 100.)
 end 
 
 type station_record = {
@@ -23,32 +27,44 @@ let _ = Random.init 85
 let temp_gen t mean = 
   7.5 *. sin((pi /. 12.) *. (t -. 6.)) +. mean
 
+(* Extract average of min.max temp *)
+let average_temp (s : station_record) = 
+  (s.data.temp_min + s.data.temp_max) / 2
+
 (* Data sensor state to JSON string *) 
 let state_to_json = function 
-  | Idle t -> `String ("idle for " ^ (string_of_int t) ^ " minutes") 
-  | Failure s -> `String ("failure - cause: " ^ s)  
-  | Collecting t -> `String ("collecting: " ^ (string_of_int t) ^ " minutes") 
-  | Maintenance t -> `String ("maintenance period:  " ^ (string_of_int t) ^ " minutes")
+  | Idle t -> `Assoc [("IDLE", `Int t)]
+  | Failure s -> `Assoc [("FAILURE", `String s)]
+  | Collecting t -> `Assoc [("COLLECTING", `Int t)]
+  | Maintenance t -> `Assoc [("MAINTENANCE", `Int t)]
+
+(* Data sensor status of strings *)
+let state_of_json = function 
+  | `Assoc [("IDLE", `Int t)] -> Idle t 
+  | `Assoc [("FAILURE", `String s)] -> Failure s 
+  | `Assoc [("COLLECTING", `Int t)] -> Collecting t 
+  | `Assoc [("MAINTENANCE", `Int t)] -> Maintenance t
+  | _ -> Failure "Failed parsing status of sensor"
 
 (* Converts a station_record to json *)
 let to_json {station_id; state; date; data} = 
-    `Assoc
-      [("station_id",`String station_id); 
-       ("status", state_to_json state);
-       ("date", `Int date);
-       ("data", WeatherData.to_json data)
-      ]
+  `Assoc
+    [("station_id",`String station_id); 
+      ("state", state_to_json state);
+      ("date", `Int date);
+      ("data", WeatherData.to_json data)
+    ]
 
 let rec find x compare = function 
-  | [] -> None 
-  | (k, v) :: lst -> if compare x k then Some v else find x compare lst  
-
-let replace_if_none = 
+  | [] -> raise (Failure ("Couldn't find " ^ x))
+  | (k, v) :: lst -> if compare x k then v else find x compare lst  
 
 let of_json = function 
   | `Assoc lst -> {
-    station_id = find "station_id" String.equal lst;
-    state = 
+    station_id = Y.Util.to_string @@ find "station_id" String.equal lst;
+    state = state_of_json @@ find "state" String.equal lst;
+    date = Y.Util.to_int @@ find "date" String.equal lst; 
+    data = WeatherData.of_json @@ find "data" String.equal lst
   }
 
 (* Int to state *)
@@ -63,8 +79,8 @@ let int_to_state = function
 let make size = 
   let random_event i = 
     let t = temp_gen (float_of_int i) 10. in
-    let temp_min = t -. (Random.float 1.) in 
-    let temp_max = t +. (Random.float 1.) in
+    let temp_min = int_of_float (t -. (Random.float 1.) *. 100.) in 
+    let temp_max = int_of_float (t +. (Random.float 1.) *. 100.) in
     let state = Random.int 4 |> int_to_state in 
     let station_id = "S1" in {station_id; state; date = i; data = {temp_min; temp_max}} in 
   List.init size random_event 
--- tests/yojson/yj.ml
+++ tests/yojson/yj.ml
@@ -1,16 +1,23 @@
+module Y = Yojson.Basic
+
 let data = Datagen.make 1_000 
 
+(* Calculating moving average over a list for a given window size *)
 let moving_average win lst = 
   let rec sum lst = function 
-    | 0 -> 0. 
-    | n -> List.hd lst +. (sum List.tl (n - 1)) in 
+    | 0 -> 0 
+    | n -> List.hd lst + (sum (List.tl lst) (n - 1)) in 
   let rec loop = function 
-    | [] | lst when List.length lst < win -> [] 
-    | lst -> ((sum lst win) /. (float_of_int win)) :: loop (List.tl lst) 
-
+    | [] -> [] 
+    | lst when List.length lst < win -> [] 
+    | lst -> (float_of_int (sum lst win) /. (float_of_int win)) :: loop (List.tl lst) 
+  in 
+    loop lst 
 
 let () = 
   (* Generating JSON *)
   let json  = `List (data |> List.map Datagen.to_json) in 
   (* Parsing JSON and computing statistics *) 
-  let temps = 
+  let data = List.map Datagen.of_json (Y.Util.to_list json) in 
+  let temps = List.map Datagen.average_temp data in 
+  let _moving = moving_average 10 temps in ()
