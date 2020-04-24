ccf865f7281bfb0752f034e80eaf3350584a6b46
patricoferris <patrickferris17@gmail.com>
2020-04-24 09:04:03 +0000

    Ask for command line arguments for the programs

--- ospike.sh
+++ ospike.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 echo "What subfolder of the benchmarks do you want to run?" 
 read sub 
+echo "What command line argument would you like to pass to the program?"
+read arg 
+
 dirr="./tests/_build/cross.riscv/"$sub
 diri="./tests/_build/cross-inline.riscv/"$sub
 diro="./tests/_build/cross-original.riscv/"$sub
@@ -22,7 +25,7 @@ spike_ospike () {
   name+=".log"
   echo $2
   echo "Running spike+ospike for: " $1 
-  spike -l  --dc=128:2:64 --ic=128:2:64 $pk $1 2>&1 | ospike stdin -o $name -lower 0 -upper 0x$2 
+  spike -l  --dc=128:2:64 --ic=128:2:64 $pk $1 $arg 2>&1 | ospike stdin -o $name -lower 0 -upper 0x$2 
 }
 
 for i in $dirr*.exe; do highest=$(range $i $dirr); spike_ospike "$i" "$highest" "$dirr"; done 
